d62be8c504d5afd812b3a410966d44993dc060ed
Yusuke Kawasaki <u-suke@kawa.net>
2012-05-05 14:23:43 +0000

    Import of KAWASAKI/HTML-TagParser-0.20 from CPAN.
    gitpan-cpan-distribution: HTML-TagParser
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         KAWASAKI/HTML-TagParser-0.20.tar.gz
    gitpan-cpan-author:       KAWASAKI
    gitpan-cpan-maturity:     released

--- lib/HTML/TagParser.pm
+++ lib/HTML/TagParser.pm
@@ -10,12 +10,15 @@ Parse a HTML file and find its <title> element's value.
     my $elem = $html->getElementsByTagName( "title" );
     print "<title>", $elem->innerText(), "</title>\n" if ref $elem;
 
-Parse a HTML source and find its first <form action=""> attribute's value.
+Parse a HTML source and find its first <form action=""> attribute's value
+and find all input elements belonging to this form.
 
     my $src  = '<html><form action="hoge.cgi">...</form></html>';
     my $html = HTML::TagParser->new( $src );
     my $elem = $html->getElementsByTagName( "form" );
     print "<form action=\"", $elem->getAttribute("action"), "\">\n" if ref $elem;
+    my @first_inputs = $elem->subTree()->getElementsByTagName( "input" );
+    my $form = $first_inputs[0]->getParent();
 
 Fetch a HTML file via HTTP, and display its all <a> elements and attributes.
 
@@ -62,7 +65,7 @@ L<URI::Fetch> module is required to fetch a file.
 =head2 $html = HTML::TagParser->new( $file );
 
 If new() is called with a filename,
-this method parses a local HTML file and returns its instance 
+this method parses a local HTML file and returns its instance
 
 =head2 $html = HTML::TagParser->new( "<html>...snip...</html>" );
 
@@ -120,6 +123,43 @@ This method returns $elem's id attribute.
 
 This method returns $elem's innerText without tags.
 
+=head2 $subhtml = $elem->subTree();
+
+This method returns a new object of class HTML::Parser,
+with all the elements that are in the DOM hierarchy under $elem.
+
+=head2 $elem = $elem->nextSibling();
+
+This method returns the next sibling within the same parent.
+It returns undef when called on a closing tag or on the lastChild node
+of a parentNode.
+
+=head2 $elem = $elem->previousSibling();
+
+This method returns the previous sibling within the same parent.
+It returns undef when called on the firstChild node of a parentNode.
+
+=head2 $child_elem = $elem->firstChild();
+
+This method returns the first child node of $elem.
+It returns undef when called on a closing tag element or on a
+non-container or empty container element.
+
+=head2 $child_elems = $elem->childNodes();
+
+This method creates an array of all child nodes of $elem and returns the array by reference.
+It returns an empty array-ref [] whenever firstChild() would return undef.
+
+=head2 $child_elem = $elem->lastChild();
+
+This method returns the last child node of $elem.
+It returns undef whenever firstChild() would return undef.
+
+=head2 $parent = $elem->parentNode();
+
+This method returns the parent node of $elem.
+It returns undef when called on root nodes.
+
 =head2 $attr = $elem->attributes();
 
 This method returns a hash of $elem's all attributes.
@@ -128,41 +168,67 @@ This method returns a hash of $elem's all attributes.
 
 This method returns the value of $elem's attributes which name is $key.
 
+=head1 BUGS
+
+The HTML-Parser is simple. Methods innerText and subTree may be
+fooled by nested tags or embedded javascript code.
+
+The methods with 'Sibling', 'child' or 'Child' in their names do not cache their results.
+The most expensive ones are lastChild() and previousSibling().
+parentNode() is also expensive, but only once. It does caching.
+
+The DOM tree is read-only, as this is just a parser.
+
 =head1 INTERNATIONALIZATION
 
-This module natively understands the character encoding used in document 
+This module natively understands the character encoding used in document
 by parsing its meta element.
 
     <meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
 
-The parsed document's encoding is converted 
+The parsed document's encoding is converted
 as this class's fixed internal encoding "UTF-8".
 
-=head1 AUTHOR
+=head1 AUTHORS AND CONTRIBUTORS
 
-Yusuke Kawasaki, http://www.kawa.net/
+    drry [drry]
+    Juergen Weigert [jnw]
+    Yusuke Kawasaki [kawasaki] [kawanet]
+    Tim Wilde [twilde]
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2006-2007 Yusuke Kawasaki. All rights reserved.
-This program is free software; you can redistribute it and/or 
+The following copyright notice applies to all the files provided in this
+distribution, including binary files, unless explicitly noted otherwise.
+
+Copyright 2006-2012 Yusuke Kawasaki
+
+This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
 
 =cut
 # ----------------------------------------------------------------
 
 package HTML::TagParser;
+use 5.008_001;
 use strict;
-use Symbol;
-use Carp;
+use Symbol ();
+use Carp ();
+use Encode ();
 
-use vars qw( $VERSION );
-$VERSION = "0.16";
+our $VERSION = "0.20";
 
-my $J2E        = {qw( jis ISO-2022-JP sjis Shift_JIS euc EUC-JP ucs2 UCS2 )};
-my $E2J        = { map { lc($_) } reverse %$J2E };
 my $SEC_OF_DAY = 60 * 60 * 24;
 
+#  [000]        '/' if closing tag.
+#  [001]        tagName
+#  [002]        attributes string (with trailing /, if self-closing tag).
+#  [003]        content until next (nested) tag.
+#  [004]        attributes hash cache.
+#  [005]        innerText combined strings cache.
+#  [006]        index of matching closing tag (or opening tag, if [000]=='/')
+#  [007]        index of parent (aka container) tag.
+#
 sub new {
     my $package = shift;
     my $src     = shift;
@@ -173,7 +239,7 @@ sub new {
     if ( $src =~ m#^https?://\w# ) {
         $self->fetch( $src, @_ );
     }
-    elsif ( $src !~ m#[\<\>\|]# && -f $src ) {
+    elsif ( $src !~ m#[<>|]# && -f $src ) {
         $self->open($src);
     }
     elsif ( $src =~ /<.*>/ ) {
@@ -216,13 +282,8 @@ sub parse {
     my $txtref = ref $text ? $text : \$text;
 
     my $charset = HTML::TagParser::Util::find_meta_charset($txtref);
-    if ( !$charset && $$txtref =~ /[^\000-\177]/ ) {
-        HTML::TagParser::Util::load_jcode();
-        my ($jc) = Jcode::getcode($txtref) if $Jcode::VERSION;
-        $charset = $J2E->{$jc} if $J2E->{$jc};
-    }
     $self->{charset} ||= $charset;
-    if ($charset) {
+    if ($charset && Encode::find_encoding($charset)) {
         HTML::TagParser::Util::encode_from_to( $txtref, $charset, "utf-8" );
     }
     my $flat = HTML::TagParser::Util::html_to_flat($txtref);
@@ -330,18 +391,138 @@ sub innerText {
     return if ( defined $elem->[002] && $elem->[002] =~ m#/$# ); # <xxx/>
 
     my $tagname = $elem->[001];
+    my $closing = HTML::TagParser::Util::find_closing($flat, $cur);
     my $list    = [];
-    for ( ; $cur < $#$flat ; $cur++ ) {
+    for ( ; $cur < $closing ; $cur++ ) {
         push( @$list, $flat->[$cur]->[003] );
-        last if ( $flat->[ $cur + 1 ]->[001] eq $tagname );
     }
     my $text = join( "", grep { $_ ne "" } @$list );
-    $text =~ s/^\s+//s;
-    $text =~ s/\s+$//s;
+    $text =~ s/^\s+|\s+$//sg;
 #   $text = "" if ( $cur == $#$flat );              # end of source
     $elem->[005] = HTML::TagParser::Util::xml_unescape( $text );
 }
 
+sub subTree
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+    my $elem = $flat->[$cur];
+    return if $elem->[000];                         # </xxx>
+    my $closing = HTML::TagParser::Util::find_closing($flat, $cur);
+    my $list    = [];
+    while (++$cur < $closing)
+      {
+        push @$list, $flat->[$cur];
+      }
+
+    # allow the getElement...() methods on the returned object.
+    return bless { flat => $list }, 'HTML::TagParser';
+}
+
+
+sub nextSibling
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+    my $elem = $flat->[$cur];
+
+    return undef if $elem->[000];                         # </xxx>
+    my $closing = HTML::TagParser::Util::find_closing($flat, $cur);
+    my $next_s = $flat->[$closing+1];
+    return undef unless $next_s;
+    return undef if $next_s->[000];     # parent's </xxx>
+    return HTML::TagParser::Element->new( $flat, $closing+1 );
+}
+
+sub firstChild
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+    my $elem = $flat->[$cur];
+    return undef if $elem->[000];                         # </xxx>
+    my $closing = HTML::TagParser::Util::find_closing($flat, $cur);
+    return undef if $closing <= $cur+1;                 # no children here.
+    return HTML::TagParser::Element->new( $flat, $cur+1 );
+}
+
+sub childNodes
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+    my $child = firstChild($self);
+    return [] unless $child;    # an empty array is easier for our callers than undef
+    my @c = ( $child );
+    while (defined ($child = nextSibling($child)))
+      {
+        push @c, $child;
+      }
+    return \@c;
+}
+
+sub lastChild
+{
+    my $c = childNodes(@_);
+    return undef unless $c->[0];
+    return $c->[-1];
+}
+
+sub previousSibling
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+
+    ## This one is expensive.
+    ## We use find_closing() which walks forward.
+    ## We'd need a find_opening() which walks backwards.
+    ## So we walk backwards one by one and consult find_closing()
+    ## until we find $cur-1 or $cur.
+
+    my $idx = $cur-1;
+    while ($idx >= 0)
+      {
+        if ($flat->[$idx][000] && defined($flat->[$idx][006]))
+          {
+            $idx = $flat->[$idx][006];  # use cache for backwards skipping
+            next;
+          }
+
+        my $closing = HTML::TagParser::Util::find_closing($flat, $idx);
+        return HTML::TagParser::Element->new( $flat, $idx )
+          if defined $closing and ($closing == $cur || $closing == $cur-1);
+        $idx--;
+      }
+    return undef;
+}
+
+sub parentNode
+{
+    my $self = shift;
+    my ( $flat, $cur ) = @$self;
+
+    return HTML::TagParser::Element->new( $flat, $flat->[$cur][007]) if $flat->[$cur][007];     # cache
+
+    ##
+    ## This one is very expensive.
+    ## We use previousSibling() to walk backwards, and
+    ## previousSibling() is expensive.
+    ##
+    my $ps = $self;
+    my $first = $self;
+
+    while (defined($ps = previousSibling($ps))) { $first = $ps; }
+
+    my $parent = $first->[1] - 1;
+    return undef if $parent < 0;
+    die "parent too short" if HTML::TagParser::Util::find_closing($flat, $parent) <= $cur;
+
+    $flat->[$cur][007] = $parent;       # cache
+    return HTML::TagParser::Element->new( $flat, $parent )
+}
+
+##
+## feature:
+## self-closing tags have an additional attribute '/' => '/'.
+##
 sub attributes {
     my $self = shift;
     my ( $flat, $cur ) = @$self;
@@ -350,11 +531,11 @@ sub attributes {
     return unless defined $elem->[002];
     my $attr = {};
     while ( $elem->[002] =~ m{
-        ([^\s\=\"\']+)(\s*=\s*(?:(")(.*?)"|(')(.*?)'|([^'"\s=]+)['"]*))?
+        ([^\s="']+)(\s*=\s*(?:["']((?(?<=")(?:\\"|[^"])*?|(?:\\'|[^'])*?))["']|([^'"\s=]+)['"]*))?
     }sgx ) {
         my $key  = $1;
         my $test = $2;
-        my $val  = ( $3 ? $4 : ( $5 ? $6 : $7 ));
+        my $val  = $3 || $4;
         my $lckey = lc($key);
         if ($test) {
             $key =~ tr/A-Z/a-z/;
@@ -376,6 +557,7 @@ use strict;
 
 sub xml_unescape {
     my $str = shift;
+    return unless defined $str;
     $str =~ s/&quot;/"/g;
     $str =~ s/&lt;/</g;
     $str =~ s/&gt;/>/g;
@@ -394,14 +576,14 @@ sub read_text_file {
 }
 
 sub html_to_flat {
-    my $txtref  = shift;    # reference
-    my $flat = [];
+    my $txtref = shift;    # reference
+    my $flat   = [];
     pos($$txtref) = undef;  # reset matching position
     while ( $$txtref =~ m{
         (?:[^<]*) < (?:
             ( / )? ( [^/!<>\s"'=]+ )
             ( (?:"[^"]*"|'[^']*'|[^"'<>])+ )?
-        |   
+        |
             (!-- .*? -- | ![^\-] .*? )
         ) > ([^<]*)
     }sxg ) {
@@ -420,10 +602,70 @@ sub html_to_flat {
     $flat;
 }
 
+## returns 1 beyond the end, if not found.
+## returns undef if called on a </xxx> closing tag
+sub find_closing
+{
+  my ($flat, $cur) = @_;
+
+  return $flat->[$cur][006]        if   $flat->[$cur][006];     # cache
+  return $flat->[$cur][006] = $cur if (($flat->[$cur][002]||'') =~ m{/$});    # self-closing
+
+  my $name = $flat->[$cur][001];
+  my $pre_nest = 0;
+  ## count how many levels deep this type of tag is nested.
+  my $idx;
+  for ($idx = 0; $idx <= $cur; $idx++)
+    {
+      my $e = $flat->[$idx];
+      next unless   $e->[001] eq $name;
+      next if     (($e->[002]||'') =~ m{/$});   # self-closing
+      $pre_nest += ($e->[000]) ? -1 : 1;
+      $pre_nest = 0 if $pre_nest < 0;
+      $idx = $e->[006]-1 if !$e->[000] && $e->[006];    # use caches for skipping forward.
+    }
+  my $last_idx = $#$flat;
+
+  ## we move last_idx closer, in case this container
+  ## has not all its subcontainers closed properly.
+  my $post_nest = 0;
+  for ($idx = $last_idx; $idx > $cur; $idx--)
+    {
+      my $e = $flat->[$idx];
+      next unless    $e->[001] eq $name;
+      $last_idx = $idx-1;               # remember where a matching tag was
+      next if      (($e->[002]||'') =~ m{/$});  # self-closing
+      $post_nest -= ($e->[000]) ? -1 : 1;
+      $post_nest = 0 if $post_nest < 0;
+      last if $pre_nest <= $post_nest;
+      $idx = $e->[006]+1 if $e->[000] && defined $e->[006];     # use caches for skipping backwards.
+    }
+
+  my $nest = 1;         # we know it is not self-closing. start behind.
+
+  for ($idx = $cur+1; $idx <= $last_idx; $idx++)
+    {
+      my $e = $flat->[$idx];
+      next unless    $e->[001] eq $name;
+      next if      (($e->[002]||'') =~ m{/$});  # self-closing
+      $nest      += ($e->[000]) ? -1 : 1;
+      if ($nest <= 0)
+        {
+          die "assert </xxx>" unless $e->[000];
+          $e->[006] = $cur;     # point back to opening tag
+          return $flat->[$cur][006] = $idx;
+        }
+      $idx = $e->[006]-1 if !$e->[000] && $e->[006];    # use caches for skipping forward.
+    }
+
+  # not all closed, but cannot go further
+  return $flat->[$cur][006] = $last_idx+1;
+}
+
 sub find_meta_charset {
     my $txtref = shift;    # reference
     while ( $$txtref =~ m{
-        <meta \s ((?: [^>]+\s )? http-equiv=['"]?Content-Type [^>]+ ) >
+        <meta \s ((?: [^>]+\s )? http-equiv\s*=\s*['"]?Content-Type [^>]+ ) >
     }sxgi ) {
         my $args = $1;
         return $1 if ( $args =~ m# charset=['"]?([^'"\s/]+) #sxgi );
@@ -436,73 +678,8 @@ sub encode_from_to {
     return     if ( $from     eq "" );
     return     if ( $to       eq "" );
     return $to if ( uc($from) eq uc($to) );
-    &load_encode() if ( $] > 5.008 );
-    if ( defined $Encode::VERSION ) {
-        # 2006/11/01 FB_XMLCREF -> XMLCREF see [Jcode5 802]
-        Encode::from_to( $$txtref, $from, $to, Encode::XMLCREF() );
-    }
-    elsif ( (  uc($from) eq "ISO-8859-1"
-            || uc($from) eq "US-ASCII"
-            || uc($from) eq "LATIN-1" ) && uc($to) eq "UTF-8" ) {
-        &latin1_to_utf8($txtref);
-    }
-    else {
-        my $jfrom = &get_jcode_name($from);
-        my $jto   = &get_jcode_name($to);
-        return $to if ( uc($jfrom) eq uc($jto) );
-        if ( $jfrom && $jto ) {
-            &load_jcode();
-            if ( defined $Jcode::VERSION ) {
-                Jcode::convert( $txtref, $jto, $jfrom );
-            }
-            else {
-                Carp::croak "Jcode.pm is required: $from to $to";
-            }
-        }
-        else {
-            Carp::croak "Encode.pm is required: $from to $to";
-        }
-    }
-    $to;
-}
-
-sub latin1_to_utf8 {
-    my $txtref = shift;
-    $$txtref =~ s{
-        ([\x80-\xFF])
-    }{
-        pack( "C2" => 0xC0|(ord($1)>>6),0x80|(ord($1)&0x3F) )
-    }exg;
-}
-
-sub load_jcode {
-    return if defined $Jcode::VERSION;
-    local $@;
-    eval { require Jcode; };
-}
-
-sub load_encode {
-    return if defined $Encode::VERSION;
-    local $@;
-    eval { require Encode; };
-}
-
-sub get_jcode_name {
-    my $src = shift;
-    my $dst;
-    if ( $src =~ /^utf-?8$/i ) {
-        $dst = "utf8";
-    }
-    elsif ( $src =~ /^euc.*jp$/i ) {
-        $dst = "euc";
-    }
-    elsif ( $src =~ /^(shift.*jis|cp932|windows-31j)$/i ) {
-        $dst = "sjis";
-    }
-    elsif ( $src =~ /^iso-2022-jp/ ) {
-        $dst = "jis";
-    }
-    $dst;
+    Encode::from_to( $$txtref, $from, $to, Encode::XMLCREF() );
+    return $to;
 }
 
 # ----------------------------------------------------------------
